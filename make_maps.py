3e9d63dae76ffd05343d3a7965dc78d0d34e046a
H. Moritz Günther <hgunther@mit.edu>
2023-04-30 07:39:55 -0400

    add time slider
    The play function works, but when stepping in between there are problems with the opacity.
    The "step" (or sliding the slider with the mouse)
    can hit an intermediate stage where two colors
    are active at the same time and since we are
    using opacity, both contribute to the visible image.

--- make_maps.py
+++ make_maps.py
@@ -1,5 +1,7 @@
 import re
 import json
+import copy
+from datetime import date, timedelta
 
 # Data is downloaded from https://maps.massgis.digital.mass.gov/MassMapper/MassMapper.html
 # Using layer: Massachusetts Municipalities Multipar Polygons
@@ -41,7 +43,13 @@ print(f'Number of municipalities with stretch code (incl. those with opt-in stre
 print(f'Number of municipalities with specialized opt-in stretch code: {len(specialized_opt_in)}')
 print(f'Number of municipalities with fossil-fuel free: {len(fossil_fuel_free)}')
 
-# Now we add that information into the geojson file
+def USdate_to_ISO(datestr):
+    month, day, year = datestr.split('/')
+    return (date(int(year), int(month), int(day)) - timedelta(days=30)).isoformat()
+
+# Now we build up the geojson file with the data we want to display
+featurelist = []
+
 for town in data['features']:
     name = town['properties']['town'].title()
     town['properties']['stretchcode'] = stretch_code.get(name, 'not yet')
@@ -58,7 +66,37 @@ for town in data['features']:
         
     town['properties']['code_color'] = code_color
 
+    feature = {'type': 'Feature',
+               'geometry': town['geometry'],
+               'properties': town['properties']}
+    feature['properties']['stretchcode'] = stretch_code.get(name, 'not yet')
+    feature['properties']['optinstretchcode'] = specialized_opt_in.get(name, 'not yet')
+    feature['properties']['fossilfuel'] = 'Fossil Fuel Free prioritized community (draft)' if name in fossil_fuel_free else ''
+    basecode = copy.deepcopy(feature)
+    basecode['properties']['code_color'] = 0
+    basecode['properties']['start'] = '2009-01-01'
+    basecode['properties']['end'] = USdate_to_ISO(stretch_code.get(name, '02/01/2025'))
+    #basecode['properties']['endExclusive'] = True
+    featurelist.append(basecode)
+    if name in stretch_code:
+        stretchcode = copy.deepcopy(feature)
+        stretchcode['properties']['code_color'] = 1
+        stretchcode['properties']['start'] = USdate_to_ISO(stretch_code.get(name))
+        stretchcode['properties']['end'] = USdate_to_ISO(specialized_opt_in.get(name, '02/01/2025'))
+        #basecode['properties']['endExclusive'] = True
+        featurelist.append(stretchcode)
+        if name in specialized_opt_in:
+            stretchcode = copy.deepcopy(feature)
+            stretchcode['properties']['code_color'] = 2
+            stretchcode['properties']['start'] = USdate_to_ISO(specialized_opt_in.get(name))
+            stretchcode['properties']['end'] = '2025-01-02'
+            #basecode['properties']['endExclusive'] = True
+            featurelist.append(stretchcode)
+
+    outdata = {'type': 'FeatureCollection', 'features': featurelist}
+
+
 # And write it into docs/MA_energy_codes_town.js such that it's read directly as a javascript file
 # That way, we get a file very similar to https://leafletjs.com/examples/choropleth/
 with open('docs/MA_energy_codes_town.js', 'w') as f:
-    f.write('var townsData = ' + json.dumps(data) + ';')
+    f.write('var townsData = ' + json.dumps(outdata) + ';')
